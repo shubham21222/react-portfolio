ccc700806c8bc4b1e389acdab111e3ac9e1bc7ed
shubham21222 <shubhamraikwar@hotmail.com>
2024-01-31 13:13:24 +0530

    update package.json

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./about.css";
 import AboutImg from "../../assets/1.jpeg";
-import CV from "../../assets/Example-Cv.pdf";
+import CV from "../../assets/shubham_resume.pdf";
 import Info from "./Info";
 
 const About = () => {
